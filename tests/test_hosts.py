f2867f337659a0a24d1372ee7ccde75267c3f1c6
Benny Daon <benny.daon@cybereason.com>
2019-01-15 18:19:27 +0200

    A more robust host test

--- tests/test_hosts.py
+++ tests/test_hosts.py
@@ -5,4 +5,4 @@ from bbtest.hosts import LocalHost
 class TestLocalHost(unittest.TestCase):
     def test_successful_run(self):
         host = LocalHost()
-        self.assertEqual(host.run(['echo', 'Hello World']), ['Hello World'])
+        self.assertTrue(host.run(['python', '--version'])[0].startswith('Python'))
